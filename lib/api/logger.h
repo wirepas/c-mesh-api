0f55d70964c9dbed4d2d7443a9a1f0430d567b0d
Şan Güneş <180301198+sgunes-wirepas@users.noreply.github.com>
2025-06-12 12:36:01 +0300

    Runtime log level selection interface and implementation for linux
    This change introduces runtime log level selection globally or per
    logging module. Applications can use Platform_set_log_level or
    Platform_set_module_log_level functions to set log levels during
    runtime.
    
    Each logging module uses a pointer to the log level managed by the
    platform implementation to minimize runtime checks.
    
    It is possible to define the NO_BUILD_ALL_LOG_LEVELS parameter to
    prevent building of logging functions with higher levels than the
    MAX_LOG_LEVEL parameter, like before.

--- lib/api/logger.h
+++ lib/api/logger.h
@@ -27,7 +27,7 @@
  */
 
 /*
- * Both following functions are declared extern instead of a dedicated
+ * All following functions are declared extern instead of a dedicated
  * header file to avoid multiple inclusion headers for that module.
  * This Logger lib header is not a real lib as depending on parameter
  * defined before including this file: MAX_LOG_LEVEL and LOG_MODULE_NAME
@@ -56,6 +56,41 @@ extern void Platform_LOG(char level, char * module, char * format, va_list args)
 extern void Platform_print_buffer(uint8_t * buffer, int size);
 
 /**
+ * \brief   Set the log level for all registered modules and future modules
+ * \param   level
+ *          Log level as defined with macros below (for example INFO_LOG_LEVEL)
+ * \return  Non-negative value upon success,
+ *          a negative value otherwise.
+ * \note    This level will be applied to modules registered after this call
+ */
+extern int Platform_set_log_level(const int level);
+
+/**
+ * \brief   Register a logging module if needed and set its log level
+ * \param   module_name
+ *          Module name string
+ * \param   level
+ *          Log level as defined with macros below (for example INFO_LOG_LEVEL)
+ * \return  Non-negative value upon success,
+ *          a negative value otherwise.
+ */
+extern int Platform_set_module_log_level(const char *const module_name, const int level);
+
+/**
+ * \brief   Register a logging module if needed and get a pointer to its log level
+ * \param   module_name
+ *          Module name string
+ * \param   default_level
+ *          Default log level as defined with macros below (for example INFO_LOG_LEVEL)
+ * \return  Pointer to the log modules log level
+ * \note    Implementation should always return a non-NULL pointer. Upon
+ *          failure, a valid pointer to a fallback value can be returned.
+ *          If a global log level was previously set via Platform_set_log_level,
+ *          that level takes precedence over the default_level.
+ */
+extern int *Platform_get_logging_module_level(const char *const module_name, const int default_level);
+
+/**
  * Macros to define several level of Log: Debug(3), Info(2), Warning(1),
  * Error(0)
  */
@@ -80,53 +115,66 @@ extern void Platform_print_buffer(uint8_t * buffer, int size);
 #    error "No module name set for logger"
 #endif
 
+// Static pointer to this module's log level - initialized on first use
+static int *__module_log_level = NULL;
+
+// Get the module's log level, initializing the module if needed
+static inline int __get_module_log_level(void)
+{
+    if (__module_log_level == NULL)
+    {
+        __module_log_level = Platform_get_logging_module_level(LOG_MODULE_NAME, MAX_LOG_LEVEL);
+    }
+    return *__module_log_level;
+}
+
+/**
+ * Macro to generate logging functions
+ */
+#define DEFINE_LOG_FUNCTION(func_name, prefix, log_level) \
+    static inline void func_name(char * format, ...) \
+    { \
+        if (__get_module_log_level() >= log_level) \
+        { \
+            va_list arg; \
+            va_start(arg, format); \
+            Platform_LOG(prefix, LOG_MODULE_NAME, format, arg); \
+            va_end(arg); \
+        } \
+    }
+
+/**
+ * Define NO_BUILD_ALL_LOG_LEVELS to prevent building of logging functions with
+ * higher logging levels than the MAX_LOG_LEVEL for the current module. Can be
+ * used to decrease binary size.
+ */
+#if !defined(NO_BUILD_ALL_LOG_LEVELS)
+#define BUILD_ALL_LOG_LEVELS
+#endif
+
 /**
  * Helpers macros to print logs
  */
-#if MAX_LOG_LEVEL >= DEBUG_LOG_LEVEL
-static inline void LOGD(char * format, ...)
-{
-    va_list arg;
-    va_start(arg, format);
-    Platform_LOG('D', LOG_MODULE_NAME, format, arg);
-    va_end(arg);
-}
+#if defined(BUILD_ALL_LOG_LEVELS) || MAX_LOG_LEVEL >= DEBUG_LOG_LEVEL
+DEFINE_LOG_FUNCTION(LOGD, 'D', DEBUG_LOG_LEVEL)
 #else
 #    define LOGD(__log__, ...)
 #endif
 
-#if MAX_LOG_LEVEL >= INFO_LOG_LEVEL
-static inline void LOGI(char * format, ...)
-{
-    va_list arg;
-    va_start(arg, format);
-    Platform_LOG('I', LOG_MODULE_NAME, format, arg);
-    va_end(arg);
-}
+#if defined(BUILD_ALL_LOG_LEVELS) || MAX_LOG_LEVEL >= INFO_LOG_LEVEL
+DEFINE_LOG_FUNCTION(LOGI, 'I', INFO_LOG_LEVEL)
 #else
 #    define LOGI(__log__, ...)
 #endif
 
-#if MAX_LOG_LEVEL >= WARNING_LOG_LEVEL
-static inline void LOGW(char * format, ...)
-{
-    va_list arg;
-    va_start(arg, format);
-    Platform_LOG('W', LOG_MODULE_NAME, format, arg);
-    va_end(arg);
-}
+#if defined(BUILD_ALL_LOG_LEVELS) || MAX_LOG_LEVEL >= WARNING_LOG_LEVEL
+DEFINE_LOG_FUNCTION(LOGW, 'W', WARNING_LOG_LEVEL)
 #else
 #    define LOGW(__log__, ...)
 #endif
 
-#if MAX_LOG_LEVEL >= ERROR_LOG_LEVEL
-static inline void LOGE(char * format, ...)
-{
-    va_list arg;
-    va_start(arg, format);
-    Platform_LOG('E', LOG_MODULE_NAME, format, arg);
-    va_end(arg);
-}
+#if defined(BUILD_ALL_LOG_LEVELS) || MAX_LOG_LEVEL >= ERROR_LOG_LEVEL
+DEFINE_LOG_FUNCTION(LOGE, 'E', ERROR_LOG_LEVEL)
 #else
 #    define LOGE(__log__, ...)
 #endif
--- lib/platform/linux/logger.c
+++ lib/platform/linux/logger.c
@@ -6,13 +6,87 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#include <stdbool.h>
+#include <string.h>
 #include <time.h>
+
+#include <pthread.h>
+
+#define LOG_MODULE_NAME "logger"
+#include "logger.h"
 
 /* Full log level string  */
 static char DEBUG[] = "DEBUG";
 static char INFO[] = "INFO";
 static char WARNING[] = "WARNING";
 static char ERROR[] = "ERROR";
+
+// Max module name length (including null terminator)
+#define MAX_LOG_MODULE_NAME_LENGTH 32
+#define MAX_LOG_MODULE_COUNT 50
+
+struct LoggingModule
+{
+    bool is_initialized;
+    int level;
+    char name[MAX_LOG_MODULE_NAME_LENGTH];
+};
+
+static pthread_mutex_t m_logging_modules_mutex = PTHREAD_MUTEX_INITIALIZER;
+static struct LoggingModule m_logging_modules[MAX_LOG_MODULE_COUNT];
+static struct LoggingModule m_fallback_module = {
+    .is_initialized = true,
+    .level = NO_LOG_LEVEL,
+    .name = "FallbackLogger"
+};
+
+static int m_global_log_level = NO_LOG_LEVEL;
+static bool m_is_global_log_level_set = false;
+
+static bool is_logging_module_name_length_valid(const char *const module_name)
+{
+    return strnlen(module_name, MAX_LOG_MODULE_NAME_LENGTH) != MAX_LOG_MODULE_NAME_LENGTH;
+}
+
+static struct LoggingModule *get_logging_module(const char *const module_name)
+{
+    if (!is_logging_module_name_length_valid(module_name))
+    {
+        return NULL;
+    }
+
+    for (size_t i = 0; i < MAX_LOG_MODULE_COUNT; i++)
+    {
+        if (m_logging_modules[i].is_initialized &&
+            strncmp(m_logging_modules[i].name, module_name, MAX_LOG_MODULE_NAME_LENGTH) == 0)
+        {
+            return &m_logging_modules[i];
+        }
+    }
+
+    return NULL;
+}
+
+static struct LoggingModule *register_logging_module(const char *const module_name, const int default_level)
+{
+    if (!is_logging_module_name_length_valid(module_name))
+    {
+        return NULL;
+    }
+
+    for (size_t i = 0; i < MAX_LOG_MODULE_COUNT; i++)
+    {
+        if (!m_logging_modules[i].is_initialized)
+        {
+            m_logging_modules[i].is_initialized = true;
+            m_logging_modules[i].level = default_level;
+            strncpy(m_logging_modules[i].name, module_name, MAX_LOG_MODULE_NAME_LENGTH);
+            return &m_logging_modules[i];
+        }
+    }
+
+    return NULL;
+}
 
 static inline void get_timestamp(char * timestamp)
 {
@@ -86,3 +160,64 @@ void Platform_print_buffer(uint8_t * buffer, int size)
     }
     printf("\n");
 }
+
+int Platform_set_log_level(const int level)
+{
+    pthread_mutex_lock(&m_logging_modules_mutex);
+
+    m_global_log_level = level;
+    m_is_global_log_level_set = true;
+
+    for (size_t i = 0; i < MAX_LOG_MODULE_COUNT; i++)
+    {
+        if (m_logging_modules[i].is_initialized)
+        {
+            m_logging_modules[i].level = level;
+        }
+    }
+
+    pthread_mutex_unlock(&m_logging_modules_mutex);
+    return 0;
+}
+
+int Platform_set_module_log_level(const char *const module_name, const int level)
+{
+    pthread_mutex_lock(&m_logging_modules_mutex);
+
+    struct LoggingModule *module = get_logging_module(module_name);
+    if (module == NULL)
+    {
+        module = register_logging_module(module_name, level);
+        if (module == NULL)
+        {
+            pthread_mutex_unlock(&m_logging_modules_mutex);
+            return -1;
+        }
+    }
+
+    module->level = level;
+
+    pthread_mutex_unlock(&m_logging_modules_mutex);
+    return 0;
+}
+
+int *Platform_get_logging_module_level(const char *const module_name, const int default_level)
+{
+    pthread_mutex_lock(&m_logging_modules_mutex);
+
+    struct LoggingModule *module = get_logging_module(module_name);
+    if (module == NULL)
+    {
+        const int initial_level = m_is_global_log_level_set ? m_global_log_level : default_level;
+        module = register_logging_module(module_name, initial_level);
+        if (module == NULL)
+        {
+            pthread_mutex_unlock(&m_logging_modules_mutex);
+            return &m_fallback_module.level;
+        }
+    }
+
+    pthread_mutex_unlock(&m_logging_modules_mutex);
+    return &module->level;
+}
+
